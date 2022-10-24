9e6b811ebb76eb667fa4f24212e2dac29a24383c
daeaevaeaed <fischertechniklol@yahoo.com>
2022-10-24 15:03:14 +0200

    erweiterung von Ceasar mit vigenere

--- ceasar_krypt.py
+++ ceasar_krypt.py
@@ -1,27 +1,50 @@
-clear = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+import math as m
 
 
-def verschlüsseln(string, key):
+clear = "abcdefghijklmnopqrstuvwxyz"
+
+
+def verschlüsseln_c(string, key, enable):
+    if enable:
+        key1 = int(key[0])
+        key2 = int(key[1])
+        key3 = int(key[2])
+    else:
+        key1 = -int(key[0])
+        key2 = -int(key[1])
+        key3 = -int(key[2])
+    # print(key1, key2, key3, "KEYS")
+    counter = 0
     final_str = ""
     for a in string:
-        # a = a.lower()
         if a == " ":
             final_str += " "
             continue
-        if a.isupper():
-            print("upper")
-            final_str += clear[(clear.index(a.lower()) + key) % 26].upper()
-        else:
-            final_str += clear[(clear.index(a) + key) % 26]
-    print(final_str)
+        mod = counter % 3
+        # print(mod)
+        if mod == 0:
+            if a.isupper():
+                final_str += clear[(clear.index(a.lower()) + key1) % 26].upper()
+            else:
+                final_str += clear[(clear.index(a) + key1) % 26]
+        if mod == 1:
+            if a.isupper():
+                final_str += clear[(clear.index(a.lower()) + key2) % 26].upper()
+            else:
+                final_str += clear[(clear.index(a) + key2) % 26]
+        if mod == 2:
+            if a.isupper():
+                final_str += clear[(clear.index(a.lower()) + key3) % 26].upper()
+            else:
+                final_str += clear[(clear.index(a) + key3) % 26]
+        # print(final_str)
+        counter += 1
+    # print(final_str)
     return final_str
 
+def entschüsseln_c(string, key, enable):
+    return verschlüsseln_c(string, key, enable)
 
-
-def entschüsseln(string, key):
-    return verschlüsseln(string, -key)
-
-# print(verschlüsseln("hallo", 2))
-# key = int(input("Welchen Schlüssel möchtest du anwenden"))
-key = int(input("key"))
-print(entschüsseln(verschlüsseln("Guten Tag", key), key), "final")
+text = verschlüsseln_c("Ich bin ein Baum", "132", True)
+print(text)
+print(entschüsseln_c(text, "132", False))
